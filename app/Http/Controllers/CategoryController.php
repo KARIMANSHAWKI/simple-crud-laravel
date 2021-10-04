4f129cd8f1d53c3bb770c34ac001d21fa44e9531
KARIMANSHAWKI <kariman0800@gmail.com>
2021-10-04 16:47:41 +0200

    adding category migration, model, controller, factory, route

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,82 +4,74 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Traits\ErrorResponse;
+use App\Traits\SuccessResponse;
+use App\Http\Requests\CategoryRequest;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+   // Traits
+   use ErrorResponse;
+   use SuccessResponse;
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+   public function index()
+   {
+       $categories = Category::all();
+       return $this->successResponse($categories);
+   }/*
+       end of list categories
+   */
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Category $category)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Category $category)
+    public function store(CategoryRequest $request)
     {
-        //
-    }
+        $data = Category::create($request->all());
+        if ($data) {
+            return $this->successResponse($data);
+        } else {
+            return $this->errorResponse();
+        }
+    }/*
+        end of store a category
+    */
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Category $category)
-    {
-        //
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Category $category)
+    public function show($id)
     {
-        //
-    }
+        $category = Category::where("id", $id)->first();
+        if ($category) {
+            return $this->successResponse($category);
+        }
+
+        return $this->errorResponse();
+    }/*
+        end of show a $category
+    */
+
+
+    public function update(CategoryRequest $request, $id)
+    {
+        $category = Category::where("id", $id)->first();
+        if ($category) {
+            $result = $category->update($request->all());
+            return $this->successResponse($result);
+        }
+
+        return $this->errorResponse();
+    }/*
+        end of update a ca$category
+    */
+
+    public function destroy($id)
+    {
+            $category = Category::where("id", $id)->first();
+            if($category){
+                $result = $category->delete();
+                return $this->successResponse($result);
+            }
+            return $this->errorResponse();
+    }/*
+        end of delete a category
+    */
 }
